7e7c43c0a7e8731511864095b68f61d5d11a674e
Josh Haimson <jdhaimson@gmail.com>
2013-03-08 17:58:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emtest::Application.config.secret_token = '55a3038746a01f09e89db7fd699299d54e03642fd25d288bf96506131251c1f46dc84924c1572e2e3cf1b457909d2b15b6e1b1e2d2a0688b91483b14e2487af9'
